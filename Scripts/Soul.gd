cf763cbefa7e54fb5f563661a55098d8dd661f21
CTausch <coletauscher@gmail.com>
2024-11-17 11:52:19 -0600

    Added Soul

--- Scripts/Soul.gd
+++ Scripts/Soul.gd
@@ -3,7 +3,7 @@ extends Node3D
 var soulIndex = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$AnimationPlayer.play("Float")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
